aa7f9433aa9337e74acaba55f8d6438e6ae85baf
Robert Poreba <robert.poreba@tukano.com.pl>
2023-04-16 12:18:52 +0200

    feat: configure eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,42 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard-with-typescript'
+  ],
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'], // Your TypeScript files extension
+
+      // As mentioned in the comments, you should extend TypeScript plugins here,
+      // instead of extending them outside the `overrides`.
+      // If you don't want to extend any rules, you don't need an `extends` attribute.
+      extends: [
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking'
+      ],
+
+      parserOptions: {
+        project: ['./tsconfig.json'] // Specify it only for TypeScript files
+      }
+    }
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: [
+    'react', '@typescript-eslint'
+  ],
+  rules: {
+    '@typescript-eslint/quotes': 'off',
+    '@typescript-eslint/semi': 'off',
+    'import/no-absolute-path': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/space-before-function-paren': 'off',
+    'react/react-in-jsx-scope': 'off'
+  }
+}
